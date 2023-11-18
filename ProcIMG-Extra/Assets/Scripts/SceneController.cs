1db6e1555acbb700210c962aa7ec03cb67cf24ca
Wesley Santos <wesleysotnas64@gmail.com>
2023-11-18 16:37:06 -0300

    feat: Desenhando na tela.

--- ProcIMG-Extra/Assets/Scripts/SceneController.cs
+++ ProcIMG-Extra/Assets/Scripts/SceneController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -14,44 +15,114 @@ public class SceneController : MonoBehaviour
     public Texture2D baseTexture;
     public Image targetImage;
 
+    [Header("Coordenates")]
+    public int pixelPositionX;
+    public int pixelPositionY;
+
+    [Header("Brush")]
+    public int brushSize;
+    public Color brushColor;
+
+    public Slider sldrBrushSize;
+    public Slider sldrBrushColorRed;
+    public Slider sldrBrushColorGreen;
+    public Slider sldrBrushColorBlue;
+    public TMP_Text txtBrsuhSize;  
+    public TMP_Text txtBrsuhColorRed;
+    public TMP_Text txtBrsuhColorGreen;
+    public TMP_Text txtBrushColorBlue;
+
     private RectTransform imageRectTransform;
 
     private void Start()
     {
         // Obtém o RectTransform da Image
         imageRectTransform = targetImage.GetComponent<RectTransform>();
+        baseTexture = CreateWhiteTexture(500, 500);
+        brushColor = new Color(0, 0, 0, 1);
+        UpdateInterface();
         RenderImage();
     }
 
     private void Update()
     {
         MousePosition();
+        Draw();
+        
+    }
 
-        // Verifica se o botão do mouse foi pressionado
-        if (Input.GetMouseButtonDown(0))
+    public void UpdateInterface()
+    {
+        brushSize = (int) sldrBrushSize.value;
+        txtBrsuhSize.text = brushSize.ToString();
+
+        float redValeu = sldrBrushColorRed.value;
+        float greenValeu = sldrBrushColorGreen.value;
+        float blueValeu = sldrBrushColorBlue.value;
+
+        brushColor = new Color(redValeu, greenValeu, blueValeu, 1);
+
+        txtBrsuhColorRed.text = brushColor.r.ToString();
+        txtBrsuhColorGreen.text = brushColor.g.ToString();
+        txtBrushColorBlue.text = brushColor.b.ToString();
+    }
+
+    public void Draw()
+    {
+        if(Input.GetMouseButton(0))
         {
-            // Obtém a posição do mouse na tela
-            Vector3 mousePos = Input.mousePosition;
-
-            // Converte para coordenadas locais da Image
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(imageRectTransform, mousePos, Camera.main, out Vector2 localPos);
-
-            // Converte para coordenadas da textura
-            Vector2 textureCoord = new Vector2(
-                (localPos.x + imageRectTransform.rect.width / 2) / imageRectTransform.rect.width,
-                (localPos.y + imageRectTransform.rect.height / 2) / imageRectTransform.rect.height
-            );
-
-            // Garante que as coordenadas estejam dentro dos limites da textura
-            textureCoord.x = Mathf.Clamp01(textureCoord.x);
-            textureCoord.y = Mathf.Clamp01(textureCoord.y);
-
-            // Obtém a cor do pixel clicado
-            Color targetColor = ((Texture2D)targetImage.mainTexture).GetPixelBilinear(textureCoord.x, textureCoord.y);
-
-            // Preenche a área com cores semelhantes adjacentes para branco
-            FloodFill(textureCoord, targetColor, Color.white);
+            SetPixelColor(pixelPositionX, pixelPositionY, brushColor, brushSize);   
         }
+    }
+
+    void SetPixelColor(int x, int y, Color color, int bSize)
+    {
+        // Verifique se as coordenadas estão dentro dos limites da textura
+        if (x >= 0 && x <= baseTexture.width && y >= 0 && y <= baseTexture.height)
+        {
+            // Calcule os limites da matriz com base no tamanho do pincel
+            int minX = Mathf.Max(0, x - bSize / 2);
+            int minY = Mathf.Max(0, y - bSize / 2);
+            int maxX = Mathf.Min(baseTexture.width - 1, x + bSize / 2);
+            int maxY = Mathf.Min(baseTexture.height - 1, y + bSize / 2);
+
+            // Pinte a matriz ao redor do pixel especificado
+            for (int i = minX; i <= maxX; i++)
+            {
+                for (int j = minY; j <= maxY; j++)
+                {
+                    baseTexture.SetPixel(i, j, color);
+                }
+            }
+
+            // Aplique as mudanças
+            baseTexture.Apply();
+        }
+        else
+        {
+            Debug.LogError("Coordenadas fora dos limites da textura.");
+        }
+    }
+
+    Texture2D CreateWhiteTexture(int width, int height)
+    {
+        // Crie uma nova instância de Texture2D
+        Texture2D texture = new Texture2D(width, height);
+
+        // Preencha a textura com a cor branca
+        Color[] pixels = new Color[width * height];
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = Color.white;
+        }
+
+        // Atribua os pixels à textura
+        texture.SetPixels(pixels);
+
+        // Aplique as mudanças
+        texture.Apply();
+
+        return texture;
     }
 
     public void RenderImage()
@@ -85,67 +156,14 @@ public class SceneController : MonoBehaviour
         textureCoord.y = Mathf.Clamp01(textureCoord.y);
         Color pixelColor = ((Texture2D)targetImage.mainTexture).GetPixelBilinear(textureCoord.x, textureCoord.y);
 
+        //Atualiza posição do pixel (X e Y)
+        pixelPositionX = (int)(textureCoord.x * baseTexture.width);
+        pixelPositionY = (int)(textureCoord.y * baseTexture.height);
+
         // Exibe as informações
-        txtMouseXPosition.text = "X: " + ((int)(textureCoord.x * baseTexture.width)).ToString();
-        txtMouseYPosition.text = "Y: " + ((int)(textureCoord.y * baseTexture.height)).ToString();
+        txtMouseXPosition.text = "X: " + pixelPositionX.ToString();
+        txtMouseYPosition.text = "Y: " + pixelPositionY.ToString();
         txtPixelColor.text = "Pixel Color: " + $"R: {pixelColor.r:F3}, G: {pixelColor.g:F3}, B: {pixelColor.b:F3}";
     }
 
-    private void FloodFill(Vector2 textureCoord, Color targetColor, Color fillColor)
-    {
-        Texture2D texture = (Texture2D)targetImage.mainTexture;
-        int width = texture.width;
-        int height = texture.height;
-
-        // Converte as coordenadas da textura para pixels
-        int startX = Mathf.RoundToInt(textureCoord.x * width);
-        int startY = Mathf.RoundToInt(textureCoord.y * height);
-
-        // Obtém a cor do pixel inicial
-        Color startColor = texture.GetPixel(startX, startY);
-
-        // Se a cor inicial for a mesma que a cor de destino, não há necessidade de preenchimento
-        if (startColor == fillColor || startColor != targetColor)
-        {
-            return;
-        }
-
-        // Usado para armazenar os pixels a serem verificados
-        Queue<Vector2Int> pixelsToCheck = new Queue<Vector2Int>();
-
-        // Adiciona o pixel inicial à fila
-        pixelsToCheck.Enqueue(new Vector2Int(startX, startY));
-
-        while (pixelsToCheck.Count > 0)
-        {
-            // Retira o próximo pixel da fila
-            Vector2Int currentPixel = pixelsToCheck.Dequeue();
-
-            // Obtém as coordenadas do pixel
-            int x = currentPixel.x;
-            int y = currentPixel.y;
-
-            // Verifica se o pixel está dentro dos limites da textura
-            if (x >= 0 && x < width && y >= 0 && y < height)
-            {
-                // Obtém a cor do pixel
-                Color currentColor = texture.GetPixel(x, y);
-
-                // Se a cor do pixel for a mesma que a cor inicial, preenche com a cor de destino
-                if (currentColor == targetColor)
-                {
-                    texture.SetPixel(x, y, fillColor);
-
-                    // Adiciona os pixels adjacentes à fila
-                    pixelsToCheck.Enqueue(new Vector2Int(x + 1, y));
-                    pixelsToCheck.Enqueue(new Vector2Int(x - 1, y));
-                    pixelsToCheck.Enqueue(new Vector2Int(x, y + 1));
-                    pixelsToCheck.Enqueue(new Vector2Int(x, y - 1));
-                }
-            }
-        }
-
-        // Aplica as alterações na textura
-        texture.Apply();
-    }
 }
